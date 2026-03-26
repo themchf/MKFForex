dd508b5dfb9df6c5e83d8232f9ead49c4bb8a9c8
themchf <michaelsuperhand@outlook.com>
2026-03-26 14:53:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Step 1: Fetch Free Financial Data
 // Note: You will replace 'demo' with a free API key from Alpha Vantage
 const API_KEY = '1DZBKLP8URPWHDC5'; 
-const API_URL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GLD&apikey=${739dc21c92bf475f8e590d305024929d}`;
+const API_URL = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GLD&apikey=${API_KEY}`;
 
 async function fetchMarketData() {
     try {
